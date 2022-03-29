9c2e33546ef1125c5c2a52b05c0ff9d13fff3da0
Victor Caruso <vic.caruso@gmail.com>
2022-03-29 16:27:04 -0700

    Test: Should sign a user in

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secret routes', () => {
   beforeEach(() => {
@@ -20,8 +21,16 @@ describe('top-secret routes', () => {
 
   it('Should create a new user in users table', async () => {
     const res = await request(app).post('/api/v1/users').send(dummyUser);
-
     expect(res.body).toEqual({ id: expect.any(String), email: 'dummy@defense.gov' });
   });
 
+  it('Should sign an existing user in', async () => {
+    const user = await UserService.createUser(dummyUser); // <-- create the user
+    const res = await request(app).post('/api/v1/users/sessions').send({ email: 'dummy@defense.gov', password: 'pa$$word' });
+    expect(res.body).toEqual({
+      message: 'Signed in successfully!',
+      user
+    });
+
+  });
 });
